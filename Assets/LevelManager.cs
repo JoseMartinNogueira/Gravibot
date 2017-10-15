fb1041ef14bbebcb13860d90ac58b54613413faf
josma517 <josma517@student.liu.se>
2017-10-16 00:43:06 +0200

    StartMenu

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadLevel (string name)
+	{
+		Application.LoadLevel( name);
+	}
+
+	public void quitRequest ()
+	{
+		Debug.Log("Quit requested ");
+		Application.Quit();
+	}
+}
